317caccc050142c3d51fb7bd52d04de21ecde96a
Tracy <akimisen@gmail.com>
2018-09-07 18:29:33 +0800

    tc_editor initialized

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,25 +1,71 @@
 <template> 
- <div class="hello"> 
- <h1>{{ msg }}</h1> 
- <h2>Essential Links</h2> 
- <el-button>默认按钮</el-button> 
- <el-button type="primary">主要按钮</el-button> 
- <el-button type="text">文字按钮</el-button> 
- </div> 
+  <div id="init_tc">
+    <el-select v-model="value2" placeholder="选择业务模块">
+      <el-option
+        v-for="item in options_module"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        :disabled="item.disabled">
+      </el-option>
+    </el-select>
+    <el-select v-model="value2" placeholder="选择模板" style="margin-left: 20px;">
+      <el-option
+        v-for="item in options_template"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        :disabled="item.disabled">
+      </el-option>
+    </el-select>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-input class="grid-content bg-purple" size="small" id="input_pkg" v-model="input" placeholder="输入专题名"></el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-input class="grid-content bg-purple" size="small" id="input_ver" v-model="input" placeholder="输入版本号">ver</el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-input class="grid-content bg-purple" size="small" id="input_title" v-model="input" placeholder="输入案例名"></el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-input class="grid-content bg-purple" size="small" id="input_description" v-model="input" placeholder="输入描述"></el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-input class="grid-content bg-purple" size="small" id="input_description" v-model="input" placeholder="输入案例名"></el-input>
+      </el-col>
+    </el-row>
+  </div>
 </template> 
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data(){
+      return {
+        options_module: [{
+          value: '0',
+          label: '融资融券交易'
+        }, {
+          value: '1',
+          label: '融资融券管理',        
+        }, {
+          value: '2',
+          label: '信用创新业务'
+        }],
+        options_template: [{
+          value: '0',
+          label: '柜台'
+        }, {
+          value: '1',
+          label: 'testapp',       
+        }],
+        value2: '',
+        input: ''
+      }
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -34,5 +80,31 @@ li {
 }
 a {
   color: #42b983;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
 }
 </style>
